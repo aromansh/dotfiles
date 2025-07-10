5459cdf8aa67e6ead36221957469467b593dc40d
Alejandro Roman <alejandro.roman@openloophealth.com>
2025-07-10 09:46:01 -0500

    feat: Set `nvim` to use `fish` shell
    That way `nvim` can find the command `now`, which was added in the
    previous commit.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,9 @@
 require "plugins"
 require "options"
 require "mappings"
+
+-- Dynamically find fish path and set as shell
+local fish_path = vim.fn.system('which fish'):gsub('\n', '')
+if fish_path and fish_path ~= '' then
+  vim.opt.shell = fish_path
+end
