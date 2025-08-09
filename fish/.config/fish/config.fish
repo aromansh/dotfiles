955eae80777a84fe0f92410fe44ef17ccdfd861d
Alejandro Roman <alejandro.roman@openloophealth.com>
2025-08-09 16:12:22 -0500

    refactor: move "interactive" config to `status is-interactive` block
    No need to have these configs only needed in interactive sessions parsed
    in all fish scripts.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,53 +1,53 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-alias vim=nvim
-alias v=nvim
-#alias mutt=neomutt
-#alias m=neomutt
-#alias o=offlineimap
-#alias om="o&&m"
-
-alias ga="git add"
-alias gaa="git add ."
-alias gap="git add -p"
-alias gs="git status"
-alias gl="git lg"
-alias gc="git commit"
-alias gcv="git commit -v"
-alias gcm="git commit -m"
-alias gca="git commit --amend"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias gp="git push"
-alias now='LC_TIME=de_DE.UTF-8 date "+%I:%M%p • %A %d %B"' # Output: 02:30PM • Donnerstag 10 Juli
-
-# Disable greeting
-set fish_greeting
-
 # Set default node version
 # See: https://github.com/jorgebucaran/nvm.fish#nvm_default_version
 set -u nvm_default_version lts
 
-# Vim FZF
-set -gx FZF_DEFAULT_OPTS '--reverse'
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 
-# Vim rg
-set -gx RIPGREP_CONFIG_PATH "$HOME/.dotfiles/ripgrep/ripgreprc"
+    # Disable greeting
+    set fish_greeting
 
-# fnm - Macbook pro
-#set PATH "/Users/alejandro/Library/Application Support/fnm" $PATH
-#fnm env | source
+    # Vim FZF
+    set -gx FZF_DEFAULT_OPTS '--reverse'
 
-# fnm - OpenLoop
-set PATH "/opt/homebrew/bin" $PATH
-fnm env | source
+    # Vim rg
+    set -gx RIPGREP_CONFIG_PATH "$HOME/.dotfiles/ripgrep/ripgreprc"
 
-# Haystack (commenting as instructed in https://webostv.developer.lge.com/develop/tools/removal-of-old-webos-tv-sdk)
-#set LG_WEBOS_TV_SDK_HOME "/Users/alejandro/.bin/webos-cli"
-#set WEBOS_CLI_TV "$LG_WEBOS_TV_SDK_HOME/CLI/bin"
-#set PATH $PATH $WEBOS_CLI_TV
+    alias vim=nvim
+    alias v=nvim
+    #alias mutt=neomutt
+    #alias m=neomutt
+    #alias o=offlineimap
+    #alias om="o&&m"
 
-# Brew
-set PATH $PATH /opt/homebrew/bin
+    alias ga="git add"
+    alias gaa="git add ."
+    alias gap="git add -p"
+    alias gs="git status"
+    alias gl="git lg"
+    alias gc="git commit"
+    alias gcv="git commit -v"
+    alias gcm="git commit -m"
+    alias gca="git commit --amend"
+    alias gd="git diff"
+    alias gdc="git diff --cached"
+    alias gp="git push"
+    alias now='LC_TIME=de_DE.UTF-8 date "+%I:%M%p • %A %d %B"' # Output: 02:30PM • Donnerstag 10 Juli
+
+    # fnm - Macbook pro
+    #set PATH "/Users/alejandro/Library/Application Support/fnm" $PATH
+    #fnm env | source
+
+    # fnm - OpenLoop
+    set PATH "/opt/homebrew/bin" $PATH
+    fnm env | source
+
+    # Haystack (commenting as instructed in https://webostv.developer.lge.com/develop/tools/removal-of-old-webos-tv-sdk)
+    #set LG_WEBOS_TV_SDK_HOME "/Users/alejandro/.bin/webos-cli"
+    #set WEBOS_CLI_TV "$LG_WEBOS_TV_SDK_HOME/CLI/bin"
+    #set PATH $PATH $WEBOS_CLI_TV
+
+    # Brew
+    set PATH $PATH /opt/homebrew/bin
+end
